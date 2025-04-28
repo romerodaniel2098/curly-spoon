f387fcb1fd978070c7acf0f2eed01ba957a9a514
daniel <romero.g.danielmail.com>
2025-04-28 12:52:01 -0500

    lista

--- trabajo2.py
+++ trabajo2.py
@@ -1,15 +1,15 @@
 #pedimos al usuario la edad
 
-edad=int(input("porfavor, ingresa tu edad: "))
+#edad=int(input("porfavor, ingresa tu edad: "))
 
-if edad <12:
-    print("eres un niño")
-elif edad >12 and edad <=17:
-    print ("eres un adolescente")
-elif edad >17 and edad <=59: 
-    print ("eres un adulto")
-else:
-    print("eres un adulto mayor.")
+#if edad <12:
+#    print("eres un niño")
+#elif edad >12 and edad <=17:
+#    print ("eres un adolescente")
+#elif edad >17 and edad <=59: 
+#    print ("eres un adulto")
+#else:
+#    print("eres un adulto mayor.")
     
 
 listasuplements = [
@@ -19,14 +19,24 @@ listasuplements = [
         "cantidad":10
     },
     {
-        "product": "preteina" ,
+        "product": "proteina",
         "value": 250,
         "cantidad": 20
     },
     {
-        "product": "aminoacidos"
-        "value": 80;
+        "product": "aminoacidos",
+        "value": 80,
         "cantidad": 15
-
+    },
+    {
+        "product": "mancuernas_de_250_kg",
+        "value": 50,
+        "cantidad": 100 
     },
 ]
+
+for suplemento in listasuplements:
+    print(f"producto: {suplemento["product"]}, precio:{suplemento["value"]}, cantidad:{suplemento["cantidad"]}")
+
+
+ 
