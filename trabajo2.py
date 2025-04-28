fe8eb5b2b7ee01f99622f43b4002c6827b6df455
daniel <romero.g.daniel@gmail.com>
2025-04-28 09:02:47 -0500

    lista

--- trabajo2.py
+++ trabajo2.py
@@ -10,4 +10,23 @@ elif edad >17 and edad <=59:
     print ("eres un adulto")
 else:
     print("eres un adulto mayor.")
+    
 
+listasuplements = [
+    {
+         "product": "creatina",
+        "value": 100, 
+        "cantidad":10
+    },
+    {
+        "product": "preteina" ,
+        "value": 250,
+        "cantidad": 20
+    },
+    {
+        "product": "aminoacidos"
+        "value": 80;
+        "cantidad": 15
+
+    },
+]
